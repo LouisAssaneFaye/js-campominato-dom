7e1e571d199491ab4ffd9c696de12850b3a26f0d
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-05-24 12:36:18 +0200

    creazione repo

---
+++ script.js
@@ -0,0 +1,23 @@
+const gridElement = document.getElementById('grid');
+const button = document.querySelector('button.button');
+
+button.addEventListener('click', function() {
+    for (let index = 0; index < 100; index++) {
+        const actualCell = createElement('div', 'cell');
+        gridElement.appendChild(actualCell);
+        actualCell.innerHTML = String(index + 1);
+        actualCell.addEventListener('click', function(){
+        actualCell.classList.toggle('selected');
+        console.log(String(index + 1));
+        });
+    }
+     
+    button.className = 'disabled';
+})
+
+function createElement(tagName, className){
+    const cellElement = document.createElement(tagName);
+    cellElement.className = className;
+    return cellElement;
+}
+
