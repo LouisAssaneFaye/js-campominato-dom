39653cda3d50dc911c189e691cddf4e107452333
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-05-24 19:56:16 +0200

    Esercizio completato

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const gridElement = document.getElementById('grid');
 const button = document.querySelector('button.button');
 let myarray = [];
+let myotherArray = [];
 const RandomUniqueNumber = getRandomUniqueNumber( 1, 100, 16 );
 
 button.addEventListener('click', function() {
+    myotherArray = [];
     gridElement.innerHTML = "";
     console.log(RandomUniqueNumber);
     for (let index = 0; index < 100; index++) {
@@ -11,22 +13,34 @@ button.addEventListener('click', function() {
         gridElement.appendChild(actualCell);
         actualCell.innerHTML = String(index + 1);
         actualCell.addEventListener('click', function(){
+            myotherArray.push(1);
+            const myotherArrayLength = myotherArray.length;
+            console.log(myotherArrayLength);
             myarray = [];
             for ( let i = 0 ; i < 16 ; i++) {
                 if (actualCell.innerHTML === String(RandomUniqueNumber[i])) {
                     myarray.push(i);
-                }      
+                }   
             }
             console.log(myarray);
+            console.log(myotherArray);
             if (myarray.length === 0){
                 actualCell.classList.toggle('selected');
                 console.log('non ha preso la bomba');
             } else {
                 actualCell.classList.toggle('selected2');
                 console.log('ha preso la bomba');
+                alert('Hai perso la partita! Hai preso una bomba!');
+                alert(`Il tuo numero di tentativi, ovvero il tuo punteggio è: ${myotherArrayLength}`);
+                alert('Riclicca su play in alto a destra se vuoi giocare un altra partita');
+            
             }
-            
-            
+
+            if (myotherArrayLength === 84){
+                alert('complimenti hai vinto la partita!!!');
+                alert('Riclicca su play in alto a destra se vuoi giocare un altra partita');
+            }
+
         });
     }
 })
